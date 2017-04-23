cadecdaadcf7a2bb49d3721de0be31e626dfb9e8
Kaiqiang Zhan <zkqsdsg123@outlook.com>
2017-04-23 14:29:56 +0800

    drag update all

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-document.getElementById('div1').onclick =
-function() {
-    this.style.background = 'green';
-    alert('color of div: green');
-};
+drag('div1');
+
+function drag(id) {
+    var obj = document.getElementById(id);
+
+    obj.onmousedown = function(ev) {
+        var disX = ev.clientX - this.offsetLeft,
+            disY = ev.clientY - this.offsetTop;
+
+        document.onmousemove = function(ev) {
+            var l = ev.clientX - disX,
+                t = ev.clientY - disY;
+
+            obj.style.left = l + 'px';
+            obj.style.top = t + 'px';
+        };
+
+        document.onmouseup = function() {
+            document.onmousemove = document.onmouseup = null;
+        };
+    };
+}
